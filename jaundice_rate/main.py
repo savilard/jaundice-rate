8d7fd7fb55e9aeb31635354cea70517fb1df37cf
savilard <vitaliyantonoff@gmail.com>
2023-03-28 13:55:37 +0600

    :sparkles: Compare the objectivity of the articles

--- jaundice_rate/main.py
+++ jaundice_rate/main.py
@@ -3,10 +3,38 @@ import pathlib
 
 import aiofiles
 import aiohttp
+from anyio import create_task_group
 from pymorphy2.analyzer import MorphAnalyzer
 
 from jaundice_rate import text_tools
 from jaundice_rate.adapters import SANITIZERS
+
+TEST_ARTICLES = [
+    'https://inosmi.ru/20230328/indiya-261728000.html',
+    'https://inosmi.ru/20230328/kosovo-261727700.html',
+    'https://inosmi.ru/20230328/finlyandiya-261723675.html',
+    'https://inosmi.ru/20230328/zapad-261719994.html',
+    'https://inosmi.ru/20230328/ekonomika-261701001.html',
+]
+
+
+async def process_article(session, morph, charged_words, url):
+    sanitize = SANITIZERS['inosmi_ru']
+    html = await fetch(session=session, url=url)
+    sanitized_html = sanitize(html)
+    article_words = text_tools.split_by_words(
+        morph=morph,
+        text=sanitized_html,
+    )
+    score = text_tools.calculate_jaundice_rate(
+        article_words=article_words,
+        charged_words=charged_words,
+    )
+
+    print('URL:', url)
+    print('Рейтинг:', score)
+    print('Слов в статье:', len(article_words))
+    print('-' * 10)
 
 
 async def fetch(session, url):
@@ -30,22 +58,12 @@ async def get_charged_words_from(directory: str, morph: MorphAnalyzer) -> list[s
 async def main():
     morph = MorphAnalyzer()
     charged_words_directory = 'jaundice_rate/charged_dict'
-    article_url = 'https://inosmi.ru/economic/20190629/245384784.html'
-    sanitize = SANITIZERS['inosmi_ru']
+    charged_words = await get_charged_words_from(charged_words_directory, morph)
 
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, article_url)
-
-    sanitized_html = sanitize(html)
-
-    article_words = text_tools.split_by_words(morph=morph, text=sanitized_html)
-    charged_words = await get_charged_words_from(charged_words_directory, morph)
-    rate = text_tools.calculate_jaundice_rate(
-        article_words=article_words,
-        charged_words=charged_words,
-    )
-    print(f'Рейтинг: {rate}')
-    print(f'Слов в статье: {len(article_words)}')
+        async with create_task_group() as tg:
+            for article in TEST_ARTICLES:
+                tg.start_soon(process_article, session, morph, charged_words, article)
 
 
 if __name__ == '__main__':
