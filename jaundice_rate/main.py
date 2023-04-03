c87252c889ed9f662c86096e831f657291866454
savilard <vitaliyantonoff@gmail.com>
2023-04-03 18:10:33 +0600

    :rotating_light: Make flake8 happy

--- jaundice_rate/main.py
+++ jaundice_rate/main.py
@@ -7,7 +7,6 @@ import urllib
 import aiofiles
 import aiohttp
 import anyio
-from anyio import create_task_group
 import pymorphy2
 from pymorphy2.analyzer import MorphAnalyzer
 
@@ -28,13 +27,15 @@ FETCH_TIMEOUT = 3
 
 
 class ProcessingStatus(enum.Enum):
+    """The status of the processing of the article on the above link."""
+
     OK = 'OK'
     FETCH_ERROR = 'FETCH_ERROR'
     PARSING_ERROR = 'PARSING_ERROR'
     TIMEOUT = 'TIMEOUT'
 
 
-def get_html_sanitizer_for(url):
+def get_sanitizer_for(url):
     parsed_url = urllib.parse.urlparse(url)
     domain = parsed_url.netloc
     try:
@@ -59,50 +60,73 @@ async def process_article(
         url: article url
         article_statistics:article statistics
     """
-    score, article_words = None, None
     try:
         html = await fetch(session=session, url=url)
-        sanitize = get_html_sanitizer_for(url)
-        sanitized_html = sanitize(html)
-        article_words = text_tools.split_by_words(
-            morph=morph,
-            text=sanitized_html,
-        )
-        score = text_tools.calculate_jaundice_rate(
-            article_words=article_words,
-            charged_words=charged_words,
-        )
-        status = ProcessingStatus.OK
     except aiohttp.ClientResponseError:
-        status = ProcessingStatus.FETCH_ERROR
-    except ArticleNotFound:
-        status = ProcessingStatus.PARSING_ERROR
+        article_statistics.append(
+            {'url': url, 'status': ProcessingStatus.FETCH_ERROR.value, 'score': None, 'word_count': None},
+        )
+        return
     except TimeoutError:
-        status = ProcessingStatus.TIMEOUT
+        article_statistics.append(
+            {'url': url, 'status': ProcessingStatus.TIMEOUT.value, 'score': None, 'word_count': None},
+        )
+        return
+    try:
+        sanitize = get_sanitizer_for(url)
+    except ArticleNotFound:
+        article_statistics.append(
+            {'url': url, 'status': ProcessingStatus.PARSING_ERROR.value, 'score': None, 'word_count': None},
+        )
+        return
+    sanitized_html = sanitize(html)
+    article_words = text_tools.split_by_words(morph=morph, text=sanitized_html)
+    score = text_tools.calculate_jaundice_rate(article_words=article_words, charged_words=charged_words)
 
     article_statistics.append(
-        {
-            'url': url,
-            'status': status.value,
-            'score': score,
-            'word_count': len(article_words) if article_words else None,
-        },
+        {'url': url, 'status': ProcessingStatus.OK.value, 'score': score, 'word_count': len(article_words)},
     )
 
 
-async def fetch(session, url):
+async def fetch(session: aiohttp.ClientSession, url: str) -> str:
+    """Fetch an article from her url.
+
+    Args:
+        session: aiohttp client session
+        url: article url
+
+    Returns:
+        object: article text
+    """
     async with anyio.fail_after(delay=FETCH_TIMEOUT):
         async with session.get(url) as response:
             response.raise_for_status()
             return await response.text()
 
 
-async def extract_file_content_from(filepath: pathlib.Path):
+async def extract_file_content_from(filepath: pathlib.Path) -> str:
+    """Extract file content from file.
+
+    Args:
+        filepath: the path to the file with the necessary information
+
+    Returns:
+        object: file content
+    """
     async with aiofiles.open(filepath, mode='r') as fd:
         return await fd.read()
 
 
 async def get_charged_words_from(directory: str, morph: MorphAnalyzer) -> list[str]:
+    """Get the list of charged words from the files in the specified directory.
+
+    Args:
+        directory: path to directory with files
+        morph: MorphAnalyzer
+
+    Returns:
+        object: list of charged words
+    """
     return [
         text_tools.split_by_words(morph=morph, text=await extract_file_content_from(filepath))
         for filepath in pathlib.Path(directory).glob('**/*.txt')
@@ -110,13 +134,14 @@ async def get_charged_words_from(directory: str, morph: MorphAnalyzer) -> list[s
 
 
 async def main():
+    """Entry point."""
     morph = MorphAnalyzer()
     charged_words_directory = 'jaundice_rate/charged_dict'
     charged_words = await get_charged_words_from(charged_words_directory, morph)
     article_statistics = []
 
     async with aiohttp.ClientSession() as session:
-        async with create_task_group() as tg:
+        async with anyio.create_task_group() as tg:
             for article in TEST_ARTICLES:
                 tg.start_soon(process_article, session, morph, charged_words, article, article_statistics)
 
