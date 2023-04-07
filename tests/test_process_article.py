37d9954ab0ebe60474486d1a2f332a5acfabcefa
savilard <vitaliyantonoff@gmail.com>
2023-04-07 16:13:05 +0600

    :white_check_mark: Add test_article_fetch_error

--- tests/test_process_article.py
+++ tests/test_process_article.py
@@ -1,3 +1,4 @@
+import aiohttp
 import pytest
 
 from jaundice_rate.process_articles import Article
@@ -16,3 +17,17 @@ async def test_article_parsing_error(morph):
     )
 
     assert article_stats == [Article(url=url, status='PARSING_ERROR', rate=None, word_count=None, elapsed_time=None)]
+
+
+@pytest.mark.asyncio
+async def test_article_fetch_error(morph):
+    url = 'https://inosmi.ru/economic/520190625/245384784.html'
+    charged_words = ['во-первых', 'хотеть', 'чтобы']
+
+    article_stats = await process_articles_from(
+            morph=morph,
+            urls=[url],
+            charged_words=charged_words,
+        )
+
+    assert article_stats == [Article(url=url, status='FETCH_ERROR', rate=None, word_count=None, elapsed_time=None)]
